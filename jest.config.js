2aea46130b7d6d79a6db759c26c85a7f19f1beec
Alexander <alexander.demidov.1997@gmail.com>
2024-10-30 01:28:39 +0300

    load tsconfig without import * with

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,22 @@
 import { pathsToModuleNameMapper } from 'ts-jest';
-import tsConfig from './tsconfig.app.json' with { type: "json" };
+import fs from 'fs';
+
+const tsConfigFile = fs.readFileSync('./tsconfig.app.json', 'utf-8');
+const tsConfig = JSON.parse(tsConfigFile);
 
 /**@type {import('ts-jest').JestConfigWithTsJest} */
 export default {
-  testEnvironment: "jsdom",
+  testEnvironment: 'jsdom',
   transform: {
-    "^.+\\.tsx?$": [
-      "ts-jest",
+    '^.+\\.tsx?$': [
+      'ts-jest',
       {
-        tsconfig: "tsconfig.app.json",
-      }
+        tsconfig: 'tsconfig.app.json',
+      },
     ],
   },
   roots: ['<rootDir>'],
   modulePaths: [tsConfig.compilerOptions.baseUrl],
-  moduleNameMapper: pathsToModuleNameMapper(
-    tsConfig.compilerOptions.paths),
+  moduleNameMapper: pathsToModuleNameMapper(tsConfig.compilerOptions.paths),
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
