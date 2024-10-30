31b531eb9ca7246e5699bdd0b1414aacf470d8bd
Alexander <alexander.demidov.1997@gmail.com>
2024-10-30 23:39:17 +0300

    add vk ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import './App.css'
 import { PackagesContainer } from 'npm-object/ui/PackagesContainer/PackagesContainer.tsx';
+import { AppRoot, Panel, PanelHeader, View } from '@vkontakte/vkui';
 
 function App() {
   return (
-    <>
-    <PackagesContainer />
-    </>
-  )
+    <AppRoot>
+      <View id="view" activePanel="main">
+        <Panel id="main">
+          <PanelHeader>Бесконечный список</PanelHeader>
+          <PackagesContainer />
+        </Panel>
+      </View>
+    </AppRoot>
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import '@vkontakte/vkui/dist/vkui.css';
+import App from './App.tsx';
+import { AdaptivityProvider, ConfigProvider } from '@vkontakte/vkui';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ConfigProvider>
+      <AdaptivityProvider>
+        <App />
+      </AdaptivityProvider>
+    </ConfigProvider>
   </StrictMode>,
-)
+);
