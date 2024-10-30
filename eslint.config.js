796262d542f38e7291a6eb0d4d2749db6602c465
Alexander <alexander.demidov.1997@gmail.com>
2024-10-30 15:35:17 +0300

    add eslint react

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
@@ -25,4 +26,16 @@ export default tseslint.config(
       ],
     },
   },
+  {
+    settings: { react: { version: '18.3.1' } },
+    plugins: {
+      react,
+    },
+    rules: {
+      // other rules...
+      // Enable its recommended rules
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+    },
+  }
 )
