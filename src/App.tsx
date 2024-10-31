20a666e5301c38b0fdd64ebe48285770dab6c0ce
Alexander <alexander.demidov.1997@gmail.com>
2024-10-31 17:38:37 +0300

    add total count to header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { PackagesContainer } from 'npm-object/ui/PackagesContainer/PackagesContainer.tsx';
-import { AppRoot, Panel, PanelHeader, PanelHeaderBack, View } from '@vkontakte/vkui';
+import { AppRoot, Counter, Panel, PanelHeader, PanelHeaderBack, View } from '@vkontakte/vkui';
 import { APP_VIEWS } from 'shared/constants/app.ts';
 import { useStore } from 'shared/model/store/root-store-context.ts';
 import { observer } from 'mobx-react-lite';
@@ -8,20 +8,21 @@ import EditPackage from 'npm-object/ui/EditPackage/EditPackage.tsx';
 const App = observer(() => {
   const {
     appStore: { view, setAppView },
+    npmObjectsStore: { npmObjectsCount },
   } = useStore();
   return (
     <AppRoot>
       <View id="main-view" activePanel={view}>
         <Panel id={APP_VIEWS.app}>
-          <PanelHeader>Бесконечный список</PanelHeader>
+          <PanelHeader
+            after={<Counter mode="primary">Total: {npmObjectsCount}</Counter>}
+          >Infinite-scroll</PanelHeader>
           <PackagesContainer />
         </Panel>
         <Panel id={APP_VIEWS.edit}>
-          <PanelHeader
-            before={<PanelHeaderBack
-              onClick={() => setAppView()}
-            />}
-          >Редактировать запись</PanelHeader>
+          <PanelHeader before={<PanelHeaderBack onClick={() => setAppView()} />}>
+            Edit npm object
+          </PanelHeader>
           <EditPackage />
         </Panel>
       </View>
